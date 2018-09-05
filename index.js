7c982de9c784a1977b004b87edff7d6f9d2cb333
TeamDynasty <38542144+TeamDynasty@users.noreply.github.com>
2018-09-06 01:59:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const botconfig = require("./botconfig.json")
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const superagent = require("superagent");
 
 bot.on("Online", async () => {
   console.log(`${bot.user.username} is online`);
@@ -19,8 +20,46 @@ bot.on("message", async message => {
   let args = messageArray.slice(1);
 
   if(cmd === `${prefix}help`){
-    return message.channel.send("En quoi puis-je vous aider ?");
+
+    let botcommands = new Discord.Richembed()
+    .setDescription("Comment puis-je vous aider ?")
+    .setColor("#FB0700")
+    .addField("Prefix :", ">")
+    .addField("help :", "Affiche la liste des commandes")
+    .addField("botinfo","Affiche les information sur le bot Geisha")
+
+    return message.channel.send(botcommands)
   }
-})
+
+  if(cmd === `${prefix}botinfo`){
+
+    let bicon = bot.user.displayAvatarURL;
+    let botembed = new Discord.Richembed()
+    .setDescription("Information de Geisha")
+    .setColor("#FB0700")
+    .setThumbnail(bicon)
+    .addField("Nom du bot", bot.user.username)
+    .addField("Codé en", "JavaScript")
+    .addField("Codé par", "Neotoxic")
+    .addField("Version ", "0.1")
+
+    return message.channel.send(botembed);
+  }
+
+
+});
+
+
+module.exports.run = async (bot,message,args) => {
+  let {body} = await superagent
+  .get(`https://random.dog/woof.json`);
+
+  let dogembed = new Discord.RichEmbed()
+  .setColor("#FB0700")
+  .setTitle("Chien")
+  .setImage(body.url);
+
+  message.channel.send(dogembed);
+}
 
 bot.login(process.env.BOT_TOKEN);
