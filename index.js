d625c19895f6ad5fa16e569acff9656083bc0eec
TeamDynasty <38542144+TeamDynasty@users.noreply.github.com>
2018-09-05 21:55:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,60 @@
-// ADRESSE //// https://discordapp.com/oauth2/authorize?client_id=473232322129428500&scope=bot&permissions=445440
+// INVITE //// https://discordapp.com/oauth2/authorize?client_id=473232322129428500&scope=bot&permissions=445440
 
-var Discord = require('discord.js');
-var bot = new Discord.Client();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
-// LOGIN ///////////////////////////////////////////////////
+// LOGIN///////////////////////////////////////////////////
+
 
 bot.login(process.env.BOT_TOKEN);
 
-// START ////////////////////////////////////////////////
-
-bot.on("message", message => {
-  var sender = message.author;
-  var msgu = message.content.toUpperCase();
-  var prefix = '>'
+bot.on("ready", () => {
+    console.log("Online ✔")
+    bot.user.setGame("waiting for instructions.");
+});
 
 
+// VARIABLES ///////////////////////////////////////////////////
+
+
+bot.on('message', message => {
+  let sender = message.author;
+  let msg = message.content;
+  let msgu = message.content.toUpperCase();
+  let cont = message.content.slice(prefix.length).split(" ");
+  let args = cont.slice(1);
 
 
 
-// HELP ////////////////////////////////////////////////////
+  let prefix = ">"
 
 
-  if (msgu === prefix + 'help') {
-    message.channel.send('Comment puis - je vous aider ?')
+
+  if (msgu.startsWith(prefix + 'purge')) {
+    async function purge() {
+      message.delete();
+
+      if (message.member.roles.find("name", "Développeur")) {
+        return;
+      }
+
+
+
+
+
+      if (isNaN(args[0])) {
+        message.channel.send('Aucun nombre selectionné');
+        return;
+      }
+
+      const fetched = await message.channel.fetchMessages({limit: args[0]});
+      console.log(fetched.size + ' messages trouvés, nettoyage ...');
+
+      message.channel.bulkDelete(fetched)
+      .catch(error => message.channel.send(`Erreur : ${error}`));
+    }
+
+    purge();
+
   }
-
-
-
-
-
-
-
-// BAN WORDS ////////////////////////////////////////////
-
-if (msgu.includes('cul')) {
-  message.delete();
-  message.author.send('Le mot utilisé est inapproprié ')
-}
-
-});
-
-
-// READY /////////////////////////////////////////////////
-
-bot.on('ready', () => {
-  console.log('Bot launched')
-});
+)};
