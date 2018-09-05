790d0fdd3f55446abf751be67dbad2559472732b
TeamDynasty <38542144+TeamDynasty@users.noreply.github.com>
2018-09-06 01:28:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,26 @@
-// INVITE //// https://discordapp.com/oauth2/authorize?client_id=473232322129428500&scope=bot&permissions=445440
+// CONNEXION ///////////////////////////////////////////////////
 
+const botconfig = require("./botconfig.json")
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-// LOGIN///////////////////////////////////////////////////
+bot.on("Online", async () => {
+  console.log(`${bot.user.username} is online`);
+  bot.user.setGame("attends les ordres ✔️")
+});
 
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if(cmd === `${prefix}help`){
+    return message.channel.send("En quoi puis-je vous aider ?");
+  }
+})
 
 bot.login(process.env.BOT_TOKEN);
-
-bot.on("ready", () => {
-    console.log("Online ✔")
-    bot.user.setGame("waiting for instructions.");
-});
-
-
-// VARIABLES ///////////////////////////////////////////////////
-
-
-bot.on('message', message => {
-  let sender = message.author;
-  let msg = message.content;
-  let msgu = message.content.toUpperCase();
-  let cont = message.content.slice(prefix.length).split(" ");
-  let args = cont.slice(1);
-
-
-
-  let prefix = ">"
-
-
-
-  if (msgu.startsWith(prefix + 'purge')) {
-    async function purge() {
-      message.delete();
-
-
-
-
-
-
-      if (isNaN(args[0])) {
-        message.channel.send('Aucun nombre selectionné');
-        return;
-      }
-
-      const fetched = await message.channel.fetchMessages({limit: args[0]});
-      console.log(fetched.size + ' messages trouvés, nettoyage ...');
-
-      message.channel.bulkDelete(fetched)
-      .catch(error => message.channel.send(`Erreur : ${error}`));
-    }
-
-    purge();
-
-  }
-});
