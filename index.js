779bbc6b74278a0c69c141abd58dd4a8fad1c03b
TeamDynasty <38542144+TeamDynasty@users.noreply.github.com>
2018-09-07 20:59:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,14 +21,76 @@ bot.on("message", async message => {
   if(cmd === `${prefix}help`){
 
     let cmdembed = new Discord.RichEmbed()
-    .setDescription("Comment puis-je vous aider ?")
+    .setTitle("Comment puis-je vous aider ?")
     .setColor("#fb0700")
     .addField("help", "Affiche la liste des commandes possibles")
     .addField("rules", "Affiche les règles du serveur")
     .addField("botinfo", "Affiche les informations sur Geisha")
+    .addField("links", "Affiche des liens utiles")
 
     return message.channel.send(cmdembed);
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  if(cmd === `${prefix}links`){
+
+    let linksembed = new Discord.RichEmbed()
+    .setTitle("Liens Utiles")
+    .setColor("#fb0700")
+    .addField("+-------- CRACKS --------+"," ")
+    .addField("Crack de jeux", "https://cracked-games.org/")
+    .addField("Statut des cracks des jeux", "https://crackwatch.com/")
+    .addField("+-------- COURS --------+"," ")
+    .addField("Cours de l'année", "https://github.com/TeamDynasty/IUT-Doua")
+    .addFiled("Cours des années précedentes", "https://repo.kuro.ml/courses-s1.git/")
+
+    return message.channel.send(linksembed);
+  }
+
+  if(cmd === `${prefix}rules`){
+
+    let rulesembed = new Discord.RichEmbed()
+    .setTitle("Liste des Règles du Serveur")
+    .setColor("#fb0700")
+    .addField("Ne doit pas avoir de caractère pornographique sauf dans le channel dédié.", " ")
+    .addField("Restez courtois, poli.", " ")
+    .addField("Pas de spam.", " ")
+    .addFiled("Pas de pub sur les différents chats sauf celui #pub.", " ")
+
+    return message.channel.send(rulesembed);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
