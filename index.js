dfa36cb52c76f3d58868512fe357a7476b021722
TeamDynasty <38542144+TeamDynasty@users.noreply.github.com>
2018-09-09 12:48:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const bot = new Discord.Client();
 
 bot.on("Online", async () => {
   console.log(`${bot.user.username} is online`);
-  bot.user.setActivity("attends les ordres ✔️", {type: "WATCHING"});
+  bot.user.setActivity('attends les ordres ✔️', {type: 'WATCHING'})
+  .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
+  .catch(console.error);
 });
 
 bot.on("message", async message => {
