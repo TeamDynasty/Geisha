a6e2bf696adb6c56159f0f361ed16f790409fd2e
TeamDynasty <38542144+TeamDynasty@users.noreply.github.com>
2018-09-12 11:31:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -51,6 +51,97 @@ fs.writeFile('Storage/userData.json', JSON.stringify(userData), (err) => {
   if (err) console.error(err);
 })
 
+if(cmd === `${prefix}help`){
+
+  let cmdembed = new Discord.RichEmbed()
+  .setTitle("Comment puis-je vous aider ?")
+  .setColor("#fb0700")
+  .addField("help", "Affiche la liste des commandes possibles")
+  .addField("rules", "Affiche les règles du serveur")
+  .addField("botinfo", "Affiche les informations sur Geisha")
+  .addField("links", "Affiche des liens utiles")
+  .addField("money", "Affiche ton compte en banque")
+  .addField("DAILY", "Recuperer sa récompense Quotidienne")
+  .addField("GUILD", "Affiche le Boss dans le serveur")
+  .addField("GLOBAL", "Affiche le client ayant le plus, dans tous les serveurs")
+
+
+  return message.channel.send(cmdembed);
+}
+
+
+
+
+
+
+
+
+
+
+try {
+
+
+if(cmd === `${prefix}links`){
+
+  let linksembed = new Discord.RichEmbed()
+  .setTitle("Liens Utiles")
+  .setColor("#fb0700")
+  .addField("+-------- CRACKS --------+","***")
+  .addField("Crack de jeux", "https://cracked-games.org/")
+  .addField("Statut des cracks des jeux", "https://crackwatch.com/")
+  .addField("Tous les sites débloqués", "https://unblocked.vet/")
+  .addField("+-------- COURS --------+","***")
+  .addField("Cours de l'année", "https://github.com/TeamDynasty/IUT-Doua")
+  .addField("Cours des années précedentes", "https://repo.kuro.ml/courses-s1.git/")
+
+  return message.channel.send(linksembed);
+}
+} catch(e) {
+message.reply(e.toString());
+}
+
+
+
+try {
+
+
+
+if(cmd === `${prefix}rules`){
+
+  let rulesembed = new Discord.RichEmbed()
+  .setTitle("Liste des Règles du Serveur")
+  .setColor("#fb0700")
+  .addField("Ne doit pas avoir de caractère pornographique sauf dans le channel dédié.", "***")
+  .addField("Restez courtois, poli.", "***")
+  .addField("Pas de spam.", "***")
+  .addField("Pas de pub sur les différents chats sauf celui #pub.", "***")
+
+  return message.channel.send(rulesembed);
+}
+} catch(e) {
+message.reply(e.toString());
+}
+
+
+
+
+// BOT INFO
+
+
+if(cmd === `${prefix}botinfo`){
+
+  let botembed = new Discord.RichEmbed()
+  .setDescription("Informations à propos de Geisha")
+  .setColor("#fb0700")
+  .addField("Codé en", "JavaScript")
+  .addField("Codé par", "Neotoxic")
+  .addField("Version ", "0.1")
+
+  return message.channel.send(botembed);
+}
+
+
+bot.login(process.env.BOT_TOKEN);
 
 // MONEY ///////////////////////////////////////////////////
 
@@ -168,138 +259,6 @@ message.channel.send({embed:{
   value:`${guildRichest} with ${guildRichest$}`
 }]
 }})
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  if(cmd === `${prefix}help`){
-
-    let cmdembed = new Discord.RichEmbed()
-    .setTitle("Comment puis-je vous aider ?")
-    .setColor("#fb0700")
-    .addField("help", "Affiche la liste des commandes possibles")
-    .addField("rules", "Affiche les règles du serveur")
-    .addField("botinfo", "Affiche les informations sur Geisha")
-    .addField("links", "Affiche des liens utiles")
-    .addField("money", "Affiche ton compte en banque")
-    .addField("DAILY", "Recuperer sa récompense Quotidienne")
-    .addField("GUILD", "Affiche le Boss dans le serveur")
-    .addField("GLOBAL", "Affiche le client ayant le plus, dans tous les serveurs")
-
-
-    return message.channel.send(cmdembed);
-  }
-
-
-
-
-
-
-
-
-
-
-  try {
-
-
-  if(cmd === `${prefix}links`){
-
-    let linksembed = new Discord.RichEmbed()
-    .setTitle("Liens Utiles")
-    .setColor("#fb0700")
-    .addField("+-------- CRACKS --------+","***")
-    .addField("Crack de jeux", "https://cracked-games.org/")
-    .addField("Statut des cracks des jeux", "https://crackwatch.com/")
-    .addField("Tous les sites débloqués", "https://unblocked.vet/")
-    .addField("+-------- COURS --------+","***")
-    .addField("Cours de l'année", "https://github.com/TeamDynasty/IUT-Doua")
-    .addField("Cours des années précedentes", "https://repo.kuro.ml/courses-s1.git/")
-
-    return message.channel.send(linksembed);
-  }
-} catch(e) {
-message.reply(e.toString());
-}
-
-
-
-  try {
-
-
-
-  if(cmd === `${prefix}rules`){
-
-    let rulesembed = new Discord.RichEmbed()
-    .setTitle("Liste des Règles du Serveur")
-    .setColor("#fb0700")
-    .addField("Ne doit pas avoir de caractère pornographique sauf dans le channel dédié.", "***")
-    .addField("Restez courtois, poli.", "***")
-    .addField("Pas de spam.", "***")
-    .addField("Pas de pub sur les différents chats sauf celui #pub.", "***")
-
-    return message.channel.send(rulesembed);
-  }
-} catch(e) {
-message.reply(e.toString());
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// BOT INFO
-
-
-  if(cmd === `${prefix}botinfo`){
-
-    let botembed = new Discord.RichEmbed()
-    .setDescription("Informations à propos de Geisha")
-    .setColor("#fb0700")
-    .addField("Codé en", "JavaScript")
-    .addField("Codé par", "Neotoxic")
-    .addField("Version ", "0.1")
-
-    return message.channel.send(botembed);
-  }
-
+} 
 
 });
-
-
-bot.login(process.env.BOT_TOKEN);
